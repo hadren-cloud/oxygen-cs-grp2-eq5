6212e275a7216f7e279c251fca7552ceefd2c6a9
Hadrien Ren <aikotanaka7@protonmail.com>
2023-07-05 18:07:37 -0400

    Add .env variables to tests

--- test/test.py
+++ test/test.py
@@ -2,17 +2,21 @@ import unittest
 from unittest.mock import patch
 from datetime import datetime as dt
 import sys
+import os
+from dotenv import load_dotenv
 sys.path.append('../src')  # Add the 'src' directory to the system path
 from main import Main  # Import the Main class from main.py
+
+load_dotenv('../variables.env')
 
 class test(unittest.TestCase):
     @patch('psycopg2.connect')
     def test_setup_database_initial_connection(self, mock_connect):
         db_config = {
-            'dbname': 'postgres',
-            'user': 'postgres',
-            'password': 'root',
-            'host': 'localhost',
+            'dbname': 'postgres',  # Temporarily connect to the default database
+            'user': os.getenv('DB_USER', 'postgres'),
+            'password': os.getenv('DB_PASSWORD'),
+            'host': os.getenv('DB_HOST', 'localhost'),
         }
         main = Main()
         main.setup_database()
@@ -21,10 +25,10 @@ class test(unittest.TestCase):
     @patch('psycopg2.connect')
     def test_setup_database_final_connection(self, mock_connect):
         db_config = {
-            'dbname': 'oxygencsgrp2eq5',
-            'user': 'postgres',
-            'password': 'root',
-            'host': 'localhost',
+            'dbname': os.getenv('DB_NAME', 'postgres'),  # Temporarily connect to the default database
+            'user': os.getenv('DB_USER', 'postgres'),
+            'password': os.getenv('DB_PASSWORD'),
+            'host': os.getenv('DB_HOST', 'localhost'),
         }
         main = Main()
         main.setup_database()
@@ -42,10 +46,10 @@ class test(unittest.TestCase):
 
         # Assert
         mock_connect.assert_called_with(
-            dbname=main.DATABASE,
-            user='postgres',
-            password='root',
-            host='localhost'
+            dbname= os.getenv('DB_NAME', 'postgres'),
+            user= os.getenv('DB_USER', 'postgres'),
+            password= os.getenv('DB_PASSWORD'),
+            host= os.getenv('DB_HOST', 'localhost')
         )
 
 if __name__ == "__main__":
