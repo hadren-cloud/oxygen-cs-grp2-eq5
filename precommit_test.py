919df96303e63be45ddb48c73eaa2e8c5375fa4e
Hadrien Ren <aikotanaka7@protonmail.com>
2023-07-06 15:07:21 -0400

    Create precommit_test.py

---
+++ precommit_test.py
@@ -0,0 +1,11 @@
+# precommit_test.py
+"""Module for demonstrating pre-commit hooks."""
+
+
+def main():
+    """Print a hello world message."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
